257107fe66a85da0bb5516c582d6e9a545755f70
rofe <github@rofe.com>
2022-08-20 09:59:38 +0200

    test(fragment): null check

--- test/blocks/fragment/fragment.test.js
+++ test/blocks/fragment/fragment.test.js
@@ -21,6 +21,6 @@ describe('Fragment block', () => {
     const section = document.querySelector('.section');
     await sectionLoaded(section);
     expect(section.textContent.trim()).to.equal('Hello world!');
-    expect(document.querySelector('.fragment')).to.not.exist;
+    expect(document.querySelectorAll('.fragment').length).to.equal(1);
   });
 });
