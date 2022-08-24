03f3f663d7fe13c3ee196da13c05f07282e883e9
David Nuescheler <david@nuescheler.ch>
2022-08-24 15:48:04 -0600

    chore: one more

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -17,7 +17,7 @@ const loadScript = (url, callback, type) => {
 };
 
 const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-    <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
+    <iframe src="${encodeURI(url.href)}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
       scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
     </iframe>
   </div>`;
