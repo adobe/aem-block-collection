6e0e7a4853e2df6048b44d98f319732c1ca142b1
David Nuescheler <david@nuescheler.ch>
2022-08-25 09:17:00 -0600

    chore: use same splitting

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -27,7 +27,7 @@ const embedYoutube = (url) => {
   let vid = encodeURIComponent(usp.get('v'));
   const embed = url.pathname;
   if (url.origin.includes('youtu.be')) {
-    vid = encodeURIComponent(url.pathname.substring(1));
+    vid = encodeURIComponent(url.pathname.split('/')[1]);
   }
   const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
       <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&amp;v=${vid}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allow="encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
