1bf396add6e5212ec5c9e5b06ce208f9c03081e8
Dylan Depass <dylandepass@gmail.com>
2023-01-25 16:48:16 -0500

    fix: simplify demo

--- blocks/franklin-fragment-demo/franklin-fragment-demo.js
+++ blocks/franklin-fragment-demo/franklin-fragment-demo.js
@@ -6,7 +6,5 @@ import { FranklinFragment } from '../franklin-fragment/franklin-fragment.js';
 
 export default async function decorate(block) {
   customElements.define('franklin-fragment', FranklinFragment);
-  const fragmentContainer = document.createElement('div');
-  fragmentContainer.innerHTML = /* html */'<franklin-fragment url="https://main--helix-playground--dylandepass.hlx.page/fragments/carousel.plain.html"></franklin-fragment>';
-  block.replaceWith(fragmentContainer);
+  block.innerHTML = /* html */'<franklin-fragment url="https://main--helix-playground--dylandepass.hlx.page/fragments/carousel.plain.html"></franklin-fragment>';
 }
