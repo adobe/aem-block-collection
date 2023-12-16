67639290d6fe37f8dc281c78912579843447cecb
Sean Steimer <ssteimer@adobe.com>
2023-12-15 16:40:25 -0800

    chore: video block w/ authored responsive poster

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -4,14 +4,37 @@
  * https://www.hlx.live/developer/block-collection/video
  */
 
+getVideoElement = (source, autoplay) => {
+  const video = document.createElement('video');
+  video.setAttribute('controls', '');
+  if (autoplay) video.setAttribute('autoplay', '');
+
+  const sourceEl = document.createElement('source');
+  sourceEl.setAttribute('src', source);
+  sourceEl.setAttribute('type', `video/${source.split('.').pop()}`);
+  video.append(sourceEl);
+
+  return video;
+};
+
 export default async function decorate(block) {
   const a = block.querySelector('a');
   if (a) {
-    const source = a.href;
-    block.innerHTML = `
-    <video controls>
-      <source src="${source}" type="video/${source.split('.').pop()}" >
-    </video>
-    `;
+    const pic = block.querySelector('picture');
+    if (pic) {
+      const wrapper = document.createElement('div');
+      wrapper.className = 'video-placeholder';
+      wrapper.innerHTML = '<div class="video-placeholder-play"><button title="Play"></button></div>';
+      wrapper.prepend(pic);
+      wrapper.addEventListener('click', () => {
+        block.innerHTML = '';
+        wrapper.replaceWith(getVideoElement(a.href, true));
+      });
+      block.append(wrapper);
+    } else {
+      const source = a.href;
+      block.innerHTML = '';
+      block.append(getVideoElement(source, false));
+    }
   }
 }
