7271eeb0b355565b81eefde1ffa942b569da515e
Aravindo Wingeier <wingeier@adobe.com>
2024-01-08 12:51:14 -0800

    refactor js

--- blocks/dialog/dialog.js
+++ blocks/dialog/dialog.js
@@ -1,9 +1,9 @@
-
 export default async function decorate(block) {
   const button = block.querySelector('.button-container');
   button.remove();
 
   const dialog = document.createElement('dialog');
+
   const closeButton = document.createElement('button');
   closeButton.classList.add('close-button');
   closeButton.setAttribute('aria-label', 'Close');
@@ -12,14 +12,8 @@ export default async function decorate(block) {
   closeButton.addEventListener('click', () => dialog.close());
   dialog.append(closeButton);
 
-  dialog.append(...block.childNodes);
-
-
-  block.append(dialog);
-
-
+  // close dialog on clicks outside the dialog. https://stackoverflow.com/a/70593278/79461
   dialog.addEventListener('click', (event) => {
-    // react to clicks outside the dialog. https://stackoverflow.com/a/70593278/79461
     const dialogDimensions = dialog.getBoundingClientRect();
     if (event.clientX < dialogDimensions.left || event.clientX > dialogDimensions.right
       || event.clientY < dialogDimensions.top || event.clientY > dialogDimensions.bottom) {
@@ -27,13 +21,14 @@ export default async function decorate(block) {
     }
   });
 
-  dialog.addEventListener('close', () => document.body.style.overflow = "");
+  dialog.append(...block.childNodes);
+  block.append(dialog);
 
   block.append(button);
   button.addEventListener('click', () => {
     dialog.showModal();
     document.body.style.overflow = "hidden"
   });
-
+  dialog.addEventListener('close', () => document.body.style.overflow = "");
 }
 
