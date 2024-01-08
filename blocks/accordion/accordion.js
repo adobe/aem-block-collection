528da30e4afd2b3e59af4bba5b3166c1a575af33
fkakatie <ksexton@adobe.com>
2024-01-08 11:46:29 -0800

    feat: accordion block

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,6 +4,10 @@
  * https://www.hlx.live/developer/block-collection/accordion
  */
 
+function hasWrapper(el) {
+  return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
+}
+
 function toggleAccordionContent(e) {
   const target = e.target.closest('button');
   const expanded = target.getAttribute('aria-expanded') === 'true';
@@ -16,17 +20,19 @@ function toggleAccordionContent(e) {
 }
 
 export default async function decorate(block) {
+  const lastAccordion = [...document.querySelectorAll('.accordion[data-num]')].pop();
+  const id = lastAccordion ? parseInt(lastAccordion.dataset.num, 10) + 1 : 1;
+  block.dataset.num = id;
   [...block.children].forEach((row, i) => {
     const [title, content] = row.children;
     // build accordion title
     const button = document.createElement('button');
     button.className = 'accordion-title';
-    button.id = `accordionTitle${i + 1}`;
+    button.id = `accordionTitle${id}${i + 1}`;
     button.setAttribute('type', 'button');
     button.setAttribute('aria-expanded', false);
-    button.setAttribute('aria-controls', `accordionContent${i + 1}`);
-    const titleHasWrapper = title.firstElementChild;
-    if (!titleHasWrapper) {
+    button.setAttribute('aria-controls', `accordionContent${id}${i + 1}`);
+    if (!hasWrapper(title)) {
       button.innerHTML = `<p>${title.innerHTML}</p>`;
     } else {
       button.innerHTML = title.innerHTML;
@@ -35,11 +41,10 @@ export default async function decorate(block) {
     button.addEventListener('click', toggleAccordionContent);
     // decorate accordion content
     content.className = 'accordion-content';
-    content.id = `accordionContent${i + 1}`;
+    content.id = `accordionContent${id}${i + 1}`;
     content.setAttribute('role', 'region');
-    content.setAttribute('aria-labelledby', `accordionTitle${i + 1}`);
-    const contentHasWrapper = content.firstElementChild;
-    if (!contentHasWrapper) {
+    content.setAttribute('aria-labelledby', `accordionTitle${id}${i + 1}`);
+    if (!hasWrapper(content)) {
       content.innerHTML = `<p>${content.innerHTML}</p>`;
     }
     content.hidden = true;
