4c1971179be681e7b2b6829f10c993d474269e4a
Chris Millar <chris@millr.org>
2024-02-04 09:36:17 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -156,7 +156,7 @@ async function loadPage() {
   const { searchParams } = new URL(window.location.href);
   const daPreview = searchParams.get('dapreview');
   if (daPreview) {
-    const origin = daPreview === 'local' ? 'http://localhost:3000' ? 'https://da.live';
+    const origin = daPreview === 'local' ? 'http://localhost:3000' : 'https://da.live';
     const { default: livePreview } = await import(`${origin}/scripts/dapreview.js`);
     livePreview(loadPage);
   }
