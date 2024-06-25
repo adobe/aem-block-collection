201e7940900f4bf351c25a295aadc052fc262f6b
Dirk <drudolph@adobe.com>
2024-06-25 09:20:48 +0200

    feat: add accordion def

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,9 +4,7 @@
  * https://www.hlx.live/developer/block-collection/accordion
  */
 
-function hasWrapper(el) {
-  return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
-}
+import { moveInstrumentation } from "../../scripts/scripts";
 
 export default function decorate(block) {
   [...block.children].forEach((row) => {
@@ -15,17 +13,12 @@ export default function decorate(block) {
     const summary = document.createElement('summary');
     summary.className = 'accordion-item-label';
     summary.append(...label.childNodes);
-    if (!hasWrapper(summary)) {
-      summary.innerHTML = `<p>${summary.innerHTML}</p>`;
-    }
     // decorate accordion item body
     const body = row.children[1];
     body.className = 'accordion-item-body';
-    if (!hasWrapper(body)) {
-      body.innerHTML = `<p>${body.innerHTML}</p>`;
-    }
     // decorate accordion item
     const details = document.createElement('details');
+    moveInstrumentation(row, details);
     details.className = 'accordion-item';
     details.append(summary, body);
     row.replaceWith(details);
