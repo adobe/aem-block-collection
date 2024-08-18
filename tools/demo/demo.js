45e1e13ab23ec43a165627e03c99ebdbf96ec50e
Chris Millar <cmillar@adobe.com>
2024-08-18 14:33:45 -0600

    Plugin demo

---
+++ tools/demo/demo.js
@@ -0,0 +1,26 @@
+// Import SDK
+import DA_SDK from 'https://da.live/nx/utils/sdk.js';
+
+(async function init() {
+  const { context, token, actions } = await DA_SDK;
+  Object.keys(context).forEach((key) => {
+    // Heading
+    const h3 = document.createElement('h3');
+    h3.textContent = `${key}`;
+
+    // Send button
+    const send = document.createElement('button');
+    send.textContent = `Send | ${context[key]}`;
+    send.addEventListener('click', () => { actions.sendText(context[key]); });
+
+    // Send & close
+    const close = document.createElement('button');
+    close.textContent = `Send & close | ${context[key]}`;
+    close.addEventListener('click', () => {
+        actions.sendText(context[key]);
+        actions.closeLibrary();
+    });
+
+    document.body.append(h3, send, close);
+  });
+}());
