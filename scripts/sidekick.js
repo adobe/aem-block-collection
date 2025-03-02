c779213ff66eeeb87ed1afbebebf9907447cd992
Chris Millar <cmillar@adobe.com>
2025-03-02 13:04:01 -0700

    More reliable exp loading w/ sidekick

--- scripts/sidekick.js
+++ scripts/sidekick.js
@@ -2,19 +2,24 @@
 import { NX_ORIGIN } from './scripts.js';
 
 let expMod;
+const DA_EXP = '/nx/public/plugins/exp/exp.js';
 
-async function loadExp() {
-  if (!expMod) {
-    // First import will run automatically
-    expMod = await import(`${NX_ORIGIN}/nx/public/plugins/exp/exp.js`);
+async function toggleExp() {
+  const exists = document.querySelector('#aem-sidekick-exp');
+
+  // If it doesn't exist, let module side effects run
+  if (!exists) {
+    expMod = await import(`${NX_ORIGIN}${DA_EXP}`);
     return;
   }
-  // 2nd time, call the func
+
+  // Else, cache the module here and toggle it.
+  if (!expMod) expMod = await import(`${NX_ORIGIN}${DA_EXP}`);
   expMod.default();
 }
 
 (async function sidekick() {
   const sk = document.querySelector('aem-sidekick');
   if (!sk) return;
-  sk.addEventListener('custom:experimentation', loadExp);
+  sk.addEventListener('custom:experimentation', toggleExp);
 }());
