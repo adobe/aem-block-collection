4419e4843ec8599ceb740b5c10344123f63a7909
Markus Haack <mhaack@adobe.com>
2025-03-14 11:59:38 +0100

    feat: use splitted json files, add accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -3,6 +3,7 @@
  * Recreate an accordion
  * https://www.hlx.live/developer/block-collection/accordion
  */
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   [...block.children].forEach((row) => {
@@ -16,6 +17,7 @@ export default function decorate(block) {
     body.className = 'accordion-item-body';
     // decorate accordion item
     const details = document.createElement('details');
+    moveInstrumentation(row, details);
     details.className = 'accordion-item';
     details.append(summary, body);
     row.replaceWith(details);
