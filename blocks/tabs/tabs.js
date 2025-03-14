756c303b59767e1179bf62ee6038bb1cbfa803c8
Markus Haack <mhaack@adobe.com>
2025-03-14 13:27:23 +0100

    fix: instrumentation for tabs

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,5 +1,23 @@
 // eslint-disable-next-line import/no-unresolved
 import { toClassName } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+function handleSelection(event) {
+  const { detail } = event;
+  const resource = detail?.resource;
+  if (resource) {
+    const element = document.querySelector(`[data-aue-resource="${resource}"]`);
+    const block = element.parentElement?.closest('.block[data-aue-resource]') || element?.closest('.block[data-aue-resource]');
+
+    // block.querySelectorAll('[role=tabpanel]').forEach((panel) => {
+    //   panel.setAttribute('aria-hidden', true);
+    // });
+    // const tablist = block.querySelector('.tabs-list');
+    // tablist.querySelectorAll('button').forEach((btn) => {
+    //   btn.setAttribute('aria-selected', false);
+    // });
+  }
+}
 
 export default async function decorate(block) {
   // build tablist
@@ -24,6 +42,7 @@ export default async function decorate(block) {
     const button = document.createElement('button');
     button.className = 'tabs-tab';
     button.id = `tab-${id}`;
+    moveInstrumentation(tab.parentElement, tabpanel.lastElementChild);
     button.innerHTML = tab.innerHTML;
     button.setAttribute('aria-controls', `tabpanel-${id}`);
     button.setAttribute('aria-selected', !i);
@@ -41,7 +60,10 @@ export default async function decorate(block) {
     });
     tablist.append(button);
     tab.remove();
+    moveInstrumentation(button.querySelector('p'), null);
   });
 
   block.prepend(tablist);
+
+  block.addEventListener('aue:ui-select', handleSelection);
 }
