155d175217a27c98d06d4027da3f53805de6d4aa
Markus Haack <mhaack@adobe.com>
2025-03-14 13:52:22 +0100

    fix: tabs

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-unresolved
 import { toClassName } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
 
 function handleSelection(event) {
   const { detail } = event;
@@ -8,14 +7,23 @@ function handleSelection(event) {
   if (resource) {
     const element = document.querySelector(`[data-aue-resource="${resource}"]`);
     const block = element.parentElement?.closest('.block[data-aue-resource]') || element?.closest('.block[data-aue-resource]');
+    if (element === block) {
+      return;
+    }
 
-    // block.querySelectorAll('[role=tabpanel]').forEach((panel) => {
-    //   panel.setAttribute('aria-hidden', true);
-    // });
-    // const tablist = block.querySelector('.tabs-list');
-    // tablist.querySelectorAll('button').forEach((btn) => {
-    //   btn.setAttribute('aria-selected', false);
-    // });
+    // switch tab panels
+    block.querySelectorAll('[role=tabpanel]').forEach((panel) => {
+      panel.setAttribute('aria-hidden', true);
+    });
+    element.setAttribute('aria-hidden', false);
+
+    // switch tab buttons
+    const tab = element?.id;
+    const tablist = block.querySelector('.tabs-list');
+    tablist.querySelectorAll('button').forEach((btn) => {
+      btn.setAttribute('aria-selected', false);
+    });
+    block.querySelector(`[aria-controls=${tab}]`).setAttribute('aria-selected', true);
   }
 }
 
@@ -42,7 +50,6 @@ export default async function decorate(block) {
     const button = document.createElement('button');
     button.className = 'tabs-tab';
     button.id = `tab-${id}`;
-    moveInstrumentation(tab.parentElement, tabpanel.lastElementChild);
     button.innerHTML = tab.innerHTML;
     button.setAttribute('aria-controls', `tabpanel-${id}`);
     button.setAttribute('aria-selected', !i);
@@ -60,10 +67,8 @@ export default async function decorate(block) {
     });
     tablist.append(button);
     tab.remove();
-    moveInstrumentation(button.querySelector('p'), null);
   });
 
   block.prepend(tablist);
-
   block.addEventListener('aue:ui-select', handleSelection);
 }
