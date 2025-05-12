1862f6b8519b60b12d0e2c83af398b3ba561bae7
Markus Haack <mhaack@adobe.com>
2025-05-12 18:05:19 +0200

    fix: clean up accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -3,7 +3,6 @@
  * Recreate an accordion
  * https://www.hlx.live/developer/block-collection/accordion
  */
-import { moveInstrumentation } from '../../scripts/scripts.js';
 
 function handleSelection(event) {
   const { detail } = event;
@@ -24,14 +23,14 @@ export default function decorate(block) {
     const label = row.children[0];
     const summary = document.createElement('summary');
     summary.className = 'accordion-item-label';
-    moveInstrumentation(label, summary);
+    // moveInstrumentation(label, summary);
     summary.append(...label.childNodes);
     // decorate accordion item body
     const body = row.children[1];
     body.className = 'accordion-item-body';
     // decorate accordion item
     const details = document.createElement('details');
-    moveInstrumentation(row, details);
+    // moveInstrumentation(row, details);
     details.className = 'accordion-item';
     details.append(summary, body);
     row.replaceWith(details);
