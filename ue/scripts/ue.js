dca9e26037bb10f9f5e49e3b73cc787f0321b35c
Markus Haack <mhaack@adobe.com>
2025-05-13 16:20:49 +0200

    fix: harden ue.js

--- ue/scripts/ue.js
+++ ue/scripts/ue.js
@@ -92,37 +92,42 @@ const setupUEEventHandlers = () => {
 
     if (resource) {
       const element = document.querySelector(`[data-aue-resource="${resource}"]`);
+      if (!element) {
+        return;
+      }
       const blockEl = element.parentElement?.closest('.block[data-aue-resource]') || element?.closest('.block[data-aue-resource]');
-      const block = blockEl.getAttribute('data-aue-model');
-      const index = element.getAttribute('data-slide-index');
+      if (blockEl) {
+        const block = blockEl.getAttribute('data-aue-model');
+        const index = element.getAttribute('data-slide-index');
 
-      switch (block) {
-        case 'accordion':
-          blockEl.querySelectorAll('details').forEach((details) => {
-            details.open = false;
-          });
-          element.open = true;
-          break;
-        case 'carousel':
-          if (index) {
-            showSlide(blockEl, index);
-          }
-          break;
-        case 'tabs':
-          if (element === block) {
-            return;
-          }
-          blockEl.querySelectorAll('[role=tabpanel]').forEach((panel) => {
-            panel.setAttribute('aria-hidden', true);
-          });
-          element.setAttribute('aria-hidden', false);
-          blockEl.querySelector('.tabs-list').querySelectorAll('button').forEach((btn) => {
-            btn.setAttribute('aria-selected', false);
-          });
-          blockEl.querySelector(`[aria-controls=${element?.id}]`).setAttribute('aria-selected', true);
-          break;
-        default:
-          break;
+        switch (block) {
+          case 'accordion':
+            blockEl.querySelectorAll('details').forEach((details) => {
+              details.open = false;
+            });
+            element.open = true;
+            break;
+          case 'carousel':
+            if (index) {
+              showSlide(blockEl, index);
+            }
+            break;
+          case 'tabs':
+            if (element === block) {
+              return;
+            }
+            blockEl.querySelectorAll('[role=tabpanel]').forEach((panel) => {
+              panel.setAttribute('aria-hidden', true);
+            });
+            element.setAttribute('aria-hidden', false);
+            blockEl.querySelector('.tabs-list').querySelectorAll('button').forEach((btn) => {
+              btn.setAttribute('aria-selected', false);
+            });
+            blockEl.querySelector(`[aria-controls=${element?.id}]`).setAttribute('aria-selected', true);
+            break;
+          default:
+            break;
+        }
       }
     }
   });
