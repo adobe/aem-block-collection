1c86631678d062d7763f3ac6b3f428d5d1d55e07
Stephen <sirugh@users.noreply.github.com>
2025-09-25 10:58:03 -0500

    img src updates apply to sibling picture sources (#17)
    * creates jpeg picture source
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * apply img change to srcsets within picture
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * revert aemjs change as unnecessary
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    ---------
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- ue/scripts/ue.js
+++ ue/scripts/ue.js
@@ -86,6 +86,20 @@ const setupObservers = () => {
 };
 
 const setupUEEventHandlers = () => {
+  // For each img source change, update the srcsets of the parent picture sources
+  document.addEventListener('aue:content-patch', (event) => {
+    if (event.detail.patch.name.match(/img.*\[src\]/)) {
+      const newImgSrc = event.detail.patch.value;
+      const picture = event.srcElement.querySelector('picture');
+
+      if (picture) {
+        picture.querySelectorAll('source').forEach((source) => {
+          source.setAttribute('srcset', newImgSrc);
+        });
+      }
+    }
+  });
+
   document.addEventListener('aue:ui-select', (event) => {
     const { detail } = event;
     const resource = detail?.resource;
